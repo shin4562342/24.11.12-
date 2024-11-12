d924c4ac8c666f7199a12e115c0c26962c20b715
shin4562342 <cjkjs08@naver.com>
2024-11-12 20:04:35 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+
+function App() { 
+  return (
+    <div className="container">
+
+      <div className={'a'}>
+            <img src={'https://img.freepik.com/premium-vector/united-states-flag-flat-symbol_121111-1200.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'b'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-france-flag_53876-27099.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'c'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-german-flag_53876-27101.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'d'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-singapore-flag_53876-27129.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'e'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-south-korea-flag_53876-27132.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'f'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-uk-flag_53876-18166.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'g'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-iran-flag_53876-18171.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'h'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-russia-flag_53876-27016.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'i'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-argentina-flag_53876-27120.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'j'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-italy-flag_53876-27098.jpg?ga=GA1.1.68145693.1731140485&semt=ais_hybrid'} />
+      </div>
+      <div className={'k'}>
+          <img src={'https://img.freepik.com/premium-vector/canada-flag-hd-vector_654626-127.jpg'} />
+      </div>
+      <div className={'l'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-belgium-flag_53876-27112.jpg'} />
+      </div>
+
+      <div className={'m'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-brazil-flag_53876-27017.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'n'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-china-flag_53876-27015.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'o'}>
+          <img src={'https://img.freepik.com/free-vector/illustration-india-flag_53876-27130.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'p'}>
+          <img src={'https://img.freepik.com/premium-vector/flag-taiwan-vector-illustration_6724-5365.jpg?semt=ais_hybrid'} />
+      </div>
+      <div className={'q'}>
+          <img src={'https://media.istockphoto.com/id/462911741/ja/%E3%83%99%E3%82%AF%E3%82%BF%E3%83%BC/%E6%97%A5%E6%9C%AC%E3%81%AE%E5%9B%BD%E6%97%97.jpg?s=612x612&w=0&k=20&c=Os6WBOYhBhPVJTqVqNznNy6UbZM6gt84JP_LuwuJJmE='} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
